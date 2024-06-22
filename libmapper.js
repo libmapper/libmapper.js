cda3ff8c761dda0d570a676052b814e00f2fd039
Matt Peachey <peacheym@dal.ca>
2024-06-22 13:52:27 -0300

    Make libmapperDevice and libmapperSignal instantiations awaitable. Update example usage code in index.html

--- libmapper.js
+++ libmapper.js
@@ -5,77 +5,121 @@
  */
 
 class LibmapperSignal {
-  constructor(name) {
-    this.name = name;
+  constructor(parentDev, element, signalId) {
+    this.name = element.id;
+    this.signalId = signalId;
+
+    let _self = this;
+    element.addEventListener("input", (event) => {
+      parentDev.ws.send(
+        JSON.stringify({
+          op: 2,
+          data: {
+            SignalId: _self.signalId,
+            Value: parseFloat(event.target.value),
+          },
+        })
+      );
+    });
+
+    console.log(
+      "Created new Signal with name: " + this.name + " and ID:" + this.signalId
+    );
 
     // Todo: Emit message to ws server to initialize a signal.
+  }
+
+  static async _createSignal(signalName, min, max, deviceId, sessionId) {
+    return new Promise((resolve, result) => {
+      fetch(`http://localhost:5001/devices/${deviceId}/signals`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Session-ID": sessionId.toString(),
+        },
+
+        // TODO: Confirm with backend API the correct way to call it & fix any bugs.
+        body: JSON.stringify({
+          direction: 2, // output
+          name: signalName,
+          min: min,
+          max: max,
+          type: 2,
+          vector_length: 1,
+          units: "slider steps",
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          resolve(data.signal_id);
+        });
+    });
   }
 }
 
 class LibmapperDevice {
-  constructor(name) {
-    this.wsUrl = "ws://localhost:5001/ws"
+  constructor(ws, sessionId, deviceId, name) {
+    this.ws = ws;
     this.name = name;
+    this.sessionId = sessionId;
+    this.deviceId = deviceId;
+    // Defined
     this.signals = [];
-    this.deviceId = -1;
-    this.sessionId = -1;
-    this.signalId = -1;
-    this.ws = new WebSocket(this.wsUrl);
-
-    this.ws.onopen = (event) => {
-      // this.ws.send("[libmapper] websocket connection is open");
-    };
-
-    this.ws.onmessage = async (msg) => {
-      var m = JSON.parse(msg.data);
-      console.log(m);
-      if (m.Op == 3) {
-        this.sessionId = m.Data;
-        // document.getElementById("session").innerText = m.Data;
-        // create device
-        await fetch("http://localhost:5001/devices", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Session-ID": this.sessionId.toString(),
-          },
-          body: JSON.stringify({
-            name: "SliderMapper",
-          }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            console.log("Created device ", data.device_id);
-            this.deviceId = data.device_id;
-            // document.getElementById("device").innerText = data.device_id;
-          });
-        // create signal
-        await fetch(`http://localhost:5001/devices/${this.deviceId}/signals`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "Session-ID": this.sessionId.toString(),
-          },
-          body: JSON.stringify({
-            direction: 2, // output
-            name: "Slider",
-          }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            console.log("Created signal ", data.signal_id);
-            this.signalId = data.signal_id;
-            // document.getElementById("signal").innerText = data.signal_id;
-          });
-      }
-    };
-    let _self = this
-    setTimeout(() => _self.ws.send(JSON.stringify({ op: 0 })), 1000);
   }
 
-  printSignals() {
-    console.log(this.signals);
-    this.signals.forEach((signal) => console.log(signal.name));
+  static async _makeDevice(sessionId, name) {
+    let devID = -1;
+
+    await fetch("http://localhost:5001/devices", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Session-ID": sessionId.toString(),
+      },
+      body: JSON.stringify({
+        name: this.name,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        devID = data.device_id;
+      });
+
+    return devID;
+  }
+
+  static async create(name) {
+    return new Promise((resolve, reject) => {
+      let ws = new WebSocket("http://localhost:5001/ws");
+
+      ws.onopen = () => {
+        ws.send(JSON.stringify({ op: 0 }));
+      };
+
+      ws.onmessage = (msg) => {
+        var recv = JSON.parse(msg.data);
+        console.log(msg.data);
+        if ((recv.op = 3)) resolve({ data: recv.data, webSocket: ws });
+      };
+    }).then((result_outer) => {
+      return new Promise(async (resolve, result) => {
+        let id = await LibmapperDevice._makeDevice(result_outer.data, name);
+
+        resolve(
+          new LibmapperDevice(
+            result_outer.webSocket,
+            result_outer.data,
+            id,
+            name
+          )
+        );
+      });
+    });
+  }
+
+  getSignals() {
+    return this.signals;
   }
 
   isValidElement(element) {
@@ -88,40 +132,28 @@ class LibmapperDevice {
     }
   }
 
-  addSignal(element) {
+  async addSignal(element) {
     // Debugging Statement
-    //   console.log(
-    //     element.id,
-    //     element.nodeName,
-    //     element.type,
-    //     element.min,
-    //     element.max,
-    //     element.value
-    //   );
+    console.log(
+      element.id,
+      element.nodeName,
+      element.type,
+      element.min,
+      element.max,
+      element.value
+    );
 
-    if (!this.isValidElement(element)) {
-      console.log("This is not a valid element");
-      return null;
-    }
+    // Get the signal ID we need asynchronously
+    let signalId = await LibmapperSignal._createSignal(
+      element.id,
+      element.min,
+      element.max,
+      this.deviceId,
+      this.sessionId
+    );
 
-    var _self = this; // Javascript wonkyness..
-
-    element.addEventListener("input", (event) => {
-      // _self.ws.send(event.target.value);
-
-      _self.ws.send(
-        JSON.stringify({
-          op: 2,
-          data: {
-            SignalId: _self.signalId,
-            Value: parseFloat(event.target.value),
-          },
-        })
-      );
-    });
-
-    // Initialize a signal, push to device list of signals and return
-    let newSignal = new LibmapperSignal(element.id);
+    // Instantiate a new signal with relevant data
+    let newSignal = new LibmapperSignal(this, element, signalId);
     this.signals.push(newSignal);
     return newSignal;
   }
